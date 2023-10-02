edefd9bcc195fbc44a28be7f83cd4feb9271a960
jma83 <jma96.elx@gmail.com>
2023-10-03 01:14:28 +0200

    WIP - Improve tailwind integration and header styles

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          500: "#FF6363;",
+          800: "#FF1313;",
+        },
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        mono: [
+          "Menlo",
+          "Monaco",
+          "Lucida Console",
+          "Liberation Mono",
+          "DejaVu Sans Mono",
+          "Bitstream Vera Sans Mono",
+          "Courier New",
+          "monospace",
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
