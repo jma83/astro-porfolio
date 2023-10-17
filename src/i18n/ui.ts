2263defa8161bf683dba1f817297281e9f7fd593
jma83 <jma96.elx@gmail.com>
2023-10-18 00:09:22 +0200

    Translate path section

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,8 +1,3 @@
-export const languages = {
-  es: "Spanish",
-  en: "English",
-};
-
 export const defaultLang = "es";
 export const showDefaultLang = false;
 
@@ -15,6 +10,7 @@ export const ui = {
     "nav.projects": "Proyectos",
     "nav.contact": "Contacto",
     "home.introduction": "¡Encantado! Soy",
+    // about
     "about.title": "Sobre mí",
     "about.description1":
       "Graduado en <b>Ingeniería Multimedia</b> en la <i>Universidad de Alicante</i> y en el <b>Máster en Informática Móvil</b> en la <i>Universidad Pontificia de Salamanca.</i>",
@@ -25,6 +21,50 @@ export const ui = {
     "about.list_item1": "Nombre:",
     "about.list_item2": "Fecha de nacimiento:",
     "about.list_item3": "Ciudad:",
+    // path
+    "path.title": "Trayectoria profesional",
+    "path.studies": "Estudios",
+    "path.experience": "Experiencia",
+    "path.languages": "Idiomas",
+    "path.tech": "Tecnologías",
+    "path.upsa.title": "Máster en Informática móvil",
+    "path.upsa.location": "Universidad Pontificia de Salamanca",
+    "path.upsa.description":
+      "Especializado en el desarrollo de aplicaciones Android, IOS e híbridas",
+    "path.ua.title": "Ingeniería Multimedia",
+    "path.ua.location": "Universidad Pontificia de Salamanca",
+    "path.ua.description":
+      "Especializado en desarrollo de páginas web, videojuegos y creación de contenidos multimedia (imágen, video, etc.)",
+    "path.vk.title": "Bachillerato tecnológico",
+    "path.vk.location": "Instituto IES Victoria Kent (Elche)",
+    "path.spanish.title": "Español",
+    "path.spanish.certificate": "Lengua materna",
+    "path.english.title": "Inglés",
+    "path.english.certificate": "First Certificate (B2) - Cambridge",
+    "path.valencian.title": "Valenciano",
+    "path.valencian.certificate": "Mitjà (C1) - Junta Qualificadora (JQCV)",
+    "path.mamby.title": "Desarrollador Frontend",
+    "path.mamby.location": "Mamby Investments",
+    "path.mamby.description":
+      "Desarrollo frontend de una aplicación web con Nuxt.js, Vue.js, Typescript y CSS. Aplicación de una plataforma de creación de contenido.",
+    "path.mamby.dateRange": "marzo 2021 - septiembre 2023 (2 años y 7 meses)",
+    "path.everis2.title": "Desarollador web Java",
+    "path.everis2.location": "Everis Alicante",
+    "path.everis2.description":
+      "Desarrollo y mantenimiento de aplicaciones de banca utilizando JSP, Java, Spring Webflow y Oracle SQL",
+    "path.everis2.dateRange": "junio 2020 - marzo 2021 (10 meses)",
+    "path.everis.title": "Desarollador web y host",
+    "path.everis.location": "Everis Murcia",
+    "path.everis.description":
+      "Desarrollo y mantenimiento de aplicaciones de telefonía utilizando JSP, Javascript y Java, para la parte web, y Cobol así como la gestión de bases de datos DB2 y SQL Server para la parte Host.",
+    "path.everis.dateRange": "junio 2019 - junio 2020 (1 año)",
+    "path.aire2.title": "Desarrollador web",
+    "path.aire.location": "Aire Networks",
+    "path.aire.description":
+      "Desarrollo y mantenimiento de un ERP de telefonía utilizando HTML, CSS, Javascript, PHP y gestión de bases de datos con MySQL",
+    "path.aire2.dateRange": "julio 2018 - marzo 2019 (9 meses)",
+    "path.aire.title": "Desarrollador web en prácticas",
+    "path.aire.dateRange": "mayo 2018 - julio 2018 (3 meses)",
   },
   en: {
     "layout.title": "Javier Martinez Arias | Developer | Portfolio",
@@ -34,5 +74,59 @@ export const ui = {
     "nav.projects": "Projects",
     "nav.contact": "Contact",
     "home.introduction": "Nice to meet you! I'm",
+    // about
+    "about.title": "About me",
+    "about.description1":
+      "Graduated in <b>Multimedia Engineering</b> at the <i>University of Alicante</i> and in <b>Master in Mobile Computing</b> at the <i>Pontifical University of Salamanca.</b> </i>",
+    "about.description2":
+      "Passionate about the IT world, with expectations to continue growing in the software development sector (web and mobile).",
+    "about.description3":
+      "Among my hobbies: coding, video games, traveling and sports.",
+    "about.list_item1": "Name:",
+    "about.list_item2": "Date of birth:",
+    "about.list_item3": "City:",
+    // path
+    "path.title": "Professional Path",
+    "path.studies": "Studies",
+    "path.experience": "Experience",
+    "path.tech": "Technologies",
+    "path.languages": "Languages",
+    "path.upsa.title": "Master in Mobile Computing",
+    "path.upsa.location": "Pontifical University of Salamanca",
+    "path.upsa.description":
+      "Specialized in the development of Android, IOS and hybrid applications.",
+    "path.ua.title": "Multimedia Engineering",
+    "path.ua.description":
+      "Specialized in web development, videogames and multimedia content creation (image, video, etc.)",
+    "path.ua.location": "University of Alicante",
+    "path.vk.title": "Technological bachelor",
+    "path.vk.location": "Victoria Kent High School (Elche)",
+    "path.spanish.title": "Spanish",
+    "path.spanish.certificate": "Mother tongue",
+    "path.english.title": "English",
+    "path.english.certificate": "First Certificate (B2) - Cambridge",
+    "path.valencian.title": "Valencian",
+    "path.valencian.certificate": "Mitjà (C1) - Junta Qualificadora (JQCV)",
+    "path.mamby.title": "Frontend Developer",
+    "path.mamby.location": "Mamby Investments",
+    "path.mamby.description":
+      "Frontend development of a web application with Nuxt.js, Vue.js, Typescript and CSS. Implementation of a content creation platform.",
+    "path.everis2.title": "Java Web Developer",
+    "path.everis2.location": "Everis Alicante",
+    "path.everis2.description":
+      "Development and maintenance of banking applications using JSP, Java, Spring Webflow and Oracle SQL",
+    "path.everis2.dateRange": "June 2020 - March 2021 (10 months)",
+    "path.everis.title": "Web developer and host",
+    "path.everis.location": "Everis Murcia",
+    "path.everis.description":
+      "Development and maintenance of telephony applications using JSP, Javascript and Java, for the web part, and Cobol as well as DB2 and SQL Server database management for the Host part.",
+    "path.everis.dateRange": "June 2019 - June 2020 (1 year)",
+    "path.aire2.title": "Web Developer",
+    "path.aire.location": "Aire Networks",
+    "path.aire.description":
+      "Development and maintenance of a telephony ERP using HTML, CSS, Javascript, PHP and database management with MySQL.",
+    "path.aire2.dateRange": "July 2018 - March 2019 (9 months)",
+    "path.aire.title": "Trainee Web Developer",
+    "path.aire.dateRange": "May 2018 - July 2018 (3 months)",
   },
 } as const;
---
+++ src/types/Path.ts
@@ -0,0 +1,27 @@
+export interface IconData {
+  src: string;
+  alt: string;
+  classes?: string;
+}
+
+export interface PathCardData {
+  title: string;
+  icon?: IconData;
+}
+
+export interface PathCardMainData extends PathCardData {
+  description?: string;
+  location: string;
+  dateRange: string;
+}
+
+export interface PathExperienceCardData extends PathCardMainData {
+  techIcons: IconData[];
+}
+
+export interface PathLanguageCardData {
+  title: string;
+  icon?: IconData;
+  date: string;
+  certificate: string;
+}
