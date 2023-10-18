0450f22fdc26106d71e6a38a0633d33a21572363
jma83 <jma96.elx@gmail.com>
2023-10-19 00:57:30 +0200

    Add tech logos and start horizontal scroll

---
+++ src/static/pathTechItems.ts
@@ -0,0 +1,38 @@
+import { IconData } from "../types/Path";
+
+const getPathTechItems = (): IconData[] => [
+  {
+    src: "/images/path/tech/html_logo.png",
+    alt: "HTML Logo",
+  },
+  {
+    src: "/images/path/tech/css_logo.png",
+    alt: "CSS Logo",
+  },
+  {
+    src: "/images/path/tech/js_logo.png",
+    alt: "Javascript Logo",
+  },
+  {
+    src: "/images/path/tech/ts_logo.png",
+    alt: "Typescript Logo",
+  },
+  {
+    src: "/images/path/tech/vue_logo.png",
+    alt: "Vue Logo",
+  },
+  {
+    src: "/images/path/tech/nuxt_logo.png",
+    alt: "Nuxt Logo",
+  },
+  {
+    src: "/images/path/tech/php_logo.png",
+    alt: "PHP Logo",
+  },
+  {
+    src: "/images/path/tech/java_logo.png",
+    alt: "Java Logo",
+  },
+];
+
+export default getPathTechItems;
